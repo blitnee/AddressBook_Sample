858776e31bc0f5708132a070d6a72a6fb4c7a999
blitnee <brittneelc@gmail.com>
2017-03-11 12:58:15 -0500

    feat: delete contact fx
    Create DeleteContact fx and testing

--- spec/AddressBookSpec.js
+++ spec/AddressBookSpec.js
@@ -1,12 +1,21 @@
 describe('AddressBook', function() {
 
-	it('should be able to add a contact', function() {
-		var	addressBook = new AddressBook(),
-			thisContact = new Contact();
+	beforeEach(function() {
+		addressBook = new AddressBook();
+		thisContact = new Contact();
+	});
 
+	it('should be able to add a contact', function() {
 		addressBook.addContact(thisContact);
 
 		expect(addressBook.getContact(0)).toBe(thisContact);
+	});
+
+	it('should be able to delete a contact', function() {
+		addressBook.addContact(thisContact);
+		addressBook.deleteContact(0);
+
+		expect(addressBook.getContact(0)).toBe(undefined);
 	});
 
 });
